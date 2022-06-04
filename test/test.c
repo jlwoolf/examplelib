00ffa113db5e28b18b3fce2c77bfa6c5b56de815
jlwoolf <jlwoolf@protonmail.com>
2022-06-03 23:48:50 -0600

    added vcpkg and formated readme

--- test/test.c
+++ test/test.c
@@ -2,11 +2,17 @@
 #include <examplelib.h>
 #include <stdio.h>
 
+START_TEST(examplelib_test) {
+    ck_assert_int_eq(2 + 2, 4);
+}
+END_TEST
+
 Suite *examplelib_suite(void) {
     Suite *suite;
     suite = suite_create("examplelib");
 
     TCase *tc_core = tcase_create("core");
+    tcase_add_test(tc_core, examplelib_test);
     suite_add_tcase(suite, tc_core);
 
     return suite;
